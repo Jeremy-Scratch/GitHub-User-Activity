974811ade57c57263687007a134ae5983383187c
Scratch <Jeremyscratch6@gmail.com>
2025-09-18 21:56:18 -0400

    Version-1.5

--- Program.cs
+++ Program.cs
@@ -2,17 +2,30 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Text.Json;
-using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Text;
 
 
 using var client = new HttpClient();
-string user = "Jeremy-Scratch";
-// string token = "a18c3bded88eb5dbb5c849a489412bf3";
+
+string user = "Jeremy-Scratch"; //we will ask the user for the username he wants to look up later 
+string token = ""; //DONT COMMINT WITH THIS TOKEN HARDCODED!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 string url = $"https://api.github.com/users/{user}/events";
-// client.DefaultRequestHeaders.Add("Authorization", token);
+//HEADERS 
+client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/vnd.github+json"));
+client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("chosenknight", token);
+//USER-AGENT MANDATORY FOR GITHUB
+client.DefaultRequestHeaders.UserAgent.Add(new ProductInfoHeaderValue("GitHubUserActivityProject", "1.0"));//MANDATORY FOR GITHUB
+
+var response = await client.GetAsync(url);
 // var respond = client.GetAsync(url).Result;
 // var res = respond.Content.ReadAsStringAsync().Result;
 // dynamic r = JsonSerializer.Serialize(res);
+ var enumOptions = new JsonSerializerOptions
+        {
+            WriteIndented = true, // Organize the Json File 
+        };
+string jsonSave = JsonSerializer.Serialize(response, enumOptions);
+File.WriteAllText("json.Json", jsonSave);
 
-Console.WriteLine(url);
+Console.WriteLine(response);
