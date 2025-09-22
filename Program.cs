9bd8160f0eb3035360a66d6d078707338fff5342
Scratch <Jeremyscratch6@gmail.com>
2025-09-22 18:47:29 -0400

    Version-1.7

--- Program.cs
+++ Program.cs
@@ -1,31 +1,21 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Text.Json;
 using System.Net.Http.Headers;
 using System.Text;
 
+string user = "Jeremy-Scratch"; //we will ask the user for the username he wants to look up later 
+                                //Console.WriteLine("Enter a GitHub user name...");
+                                //string user = Console.ReadLine();
+using HttpClient client = new();
 
-// using var client = new HttpClient();
+string token = ""; //DONT COMMINT WITH THIS TOKEN HARDCODED!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!                 
+//HEADERS 
+client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/vnd.github+json"));
+client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("chosenknight", token);
+client.DefaultRequestHeaders.UserAgent.Add(new ProductInfoHeaderValue("GitHubUserActivityProject", "1.0"));//MANDATORY FOR GITHUB
 
-// string user = "Jeremy-Scratch"; //we will ask the user for the username he wants to look up later 
-// string token = ""; //DONT COMMINT WITH THIS TOKEN HARDCODED!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-// string url = $"https://api.github.com/users/{user}/events";
-// //HEADERS 
-// client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/vnd.github+json"));
-// client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("chosenknight", token);
-// //USER-AGENT MANDATORY FOR GITHUB
-// client.DefaultRequestHeaders.UserAgent.Add(new ProductInfoHeaderValue("GitHubUserActivityProject", "1.0"));//MANDATORY FOR GITHUB
+var response = await client.GetAsync($"https://api.github.com/users/{user}/events");
+response.EnsureSuccessStatusCode();
+string data = await response.Content.ReadAsStringAsync();
 
-// var response = await client.GetStringAsync(url);
-
-//  var enumOptions = new JsonSerializerOptions
-//         {
-//             WriteIndented = true, // Organize the Json File 
-//         };
-// string jsonSave = JsonSerializer.Serialize(response, enumOptions);
-//  File.WriteAllText("json.Json", response);
-
-// Console.WriteLine(response);
-
-
+File.WriteAllText("json.Json", data);
