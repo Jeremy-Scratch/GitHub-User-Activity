4872f9c279adbe9a29bc61328eb1c9715db802d7
Scratch <Jeremyscratch6@gmail.com>
2025-09-23 14:33:55 -0400

    Env-Var-Implementation

--- Services/GithubEventService.cs
+++ Services/GithubEventService.cs
@@ -5,7 +5,8 @@ public class EventService
 {
     public async Task<List<Events>> Getter(string? user)
     {
-        string token = "";
+        string token = Environment.GetEnvironmentVariable("MY_TOKEN", EnvironmentVariableTarget.User) ?? throw new InvalidOperationException("GITHUB_TOKEN environment variable not set.");
+        
         using HttpClient client = new();
         client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/vnd.github+json"));
         client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("chosenknight", token);
